966cd5115d8674fc7c32ac8091fa29ad3997e396
lthanhbinh <lthanhbinh@tma.com.vn>
2018-05-06 12:49:14 +0700

    adsad

--- js/class.SiteMain.js
+++ js/class.SiteMain.js
@@ -31,11 +31,32 @@ var SiteMain = (function() {
 		  slidesToScroll: 4,
 		  arrows: true,
 		  responsive: [
+			  {
+			 breakpoint: 1200,
+			 settings: {
+			   slidesToShow: 3,
+			   slidesToScroll: 3,
+			 }
+		   },
 		    {
 		      breakpoint: 1024,
 		      settings: {
-		        slidesToShow: 4,
-		        slidesToScroll: 4,
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		      }
+		    },
+			{
+		      breakpoint: 992,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2,
+		      }
+		    },
+			{
+		      breakpoint: 768,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
 		      }
 		    },
 		    {
