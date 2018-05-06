ba19422e12e40b0c008f536214a5899b87f18eb3
lthanhbinh <lthanhbinh@tma.com.vn>
2018-05-07 00:02:43 +0700

    aaaaaaa

---
+++ luotsong.php
@@ -0,0 +1,280 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html lang="en">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<link rel="shortcut icon" href="images/favicon.ico" />
+<title>TRADE COIN CLUB</title>
+<meta name="description" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="css/all.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha256-3dkvEK0WLHRJ7/Csr0BZjAWxERc5WH7bdeUya2aXxdU= sha512-+L4yy6FRcDGbXJ9mPG8MT/3UCDzwR9gPeyFNMCtInsol++5m3bk2bXWKdZjvybmohrAsn3Ua5x8gfLnbE1YkOg==" crossorigin="anonymous">
+<style>
+  @font-face {
+	font-family: 'Glyphicons Halflings';
+	src:url('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/fonts/glyphicons-halflings-regular.eot');
+	src:url('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/fonts/glyphicons-halflings-regular.eot?#iefix') format('embedded-opentype'),
+	  url('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/fonts/glyphicons-halflings-regular.woff') format('woff'),
+	  url('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/fonts/glyphicons-halflings-regular.ttf') format('truetype'),
+	  url('https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.5/fonts/glyphicons-halflings-regular.svg#glyphicons-halflingsregular') format('svg');
+  }
+</style>
+<script src="js/jquery-1.9.1.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/slick.min.js"></script>
+<script type="text/javascript" src="js/jquery-rate-picker.js"></script>
+<script src="js/class.SiteMain.js"></script>
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+	<div class="wrapper">
+		<!--HEADER-->
+		<header class="tradeCoinHeader loggedIn">
+			<div class="bgHeader">
+                <div class="container">
+    				<a href="#" class="tradeLogo"><img src="images/tradeLogo.png"/></a>
+    				<div class="tradeSearch">
+                        <a href="#" class="infoTooltip"></a>
+                        <form action="" method="">
+                            <input type="text" value="" placeholder="" />
+                        </form>
+                    </div>
+                    <div class="userLogin">
+                        <div class="userBox">
+                            <a href="#">Username</a>
+                            <div class="userDropDown" style="display: none;">
+                                <ul>
+                                    <li><a href="#">Profile</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <a href="#" class="avatar"></a>
+                    </div>
+                    <div class="clear"></div>
+    			</div>
+            </div>
+            <div class="bgNavigation">
+                <div class="container">
+                    <ul class="navigationBar">
+                        <li class="current-menu-item"><a href="#" class="icon1">Lướt sóng</a></li>
+                        <li><a href="#" class="icon2">Trade Coin</a></li>
+                        <li><a href="#" class="icon3">Hold Coin</a></li>
+                        <li><a href="#" class="icon4">ICO</a></li>
+                    </ul>
+                </div>
+            </div>
+		</header>
+        <div class="tradeLists greyBg tradeListPage">
+			<div class="container">
+				<h3 class="commonTitle align-center">Lướt sóng</h3>
+				<div class="tradeTable">
+					<div class="tradeTitle">
+						<div class="col col1">Trạng thái</div>
+						<div class="col col2">Kèo</div>
+						<div class="col col3">Win/Loss</div>
+						<div class="col col4">Tỷ lệ</div>
+						<div class="col col5">Mod</div>
+						<div class="col col6">Đánh giá</div>
+					</div>
+					<div class="tradeContent">
+						<div class="tradeRow pink">
+							<div class="col col1"><span class="new">New</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span>...........</span></div>
+							<div class="col col4"><span>...........</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow pink">
+							<div class="col col1"><span class="active">Active</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span>...........</span></div>
+							<div class="col col4"><span>...........</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="tradeRow pink">
+							<div class="col col1"><span class="active">Active</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span>...........</span></div>
+							<div class="col col4"><span>...........</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="tradeRow pink">
+							<div class="col col1"><span class="active">Active</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span>...........</span></div>
+							<div class="col col4"><span>...........</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="tradeRow pink">
+							<div class="col col1"><span class="active">Active</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span>...........</span></div>
+							<div class="col col4"><span>...........</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow pink">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow pink">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="red">Loss</span></div>
+							<div class="col col4"><span class="red">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+						<div class="tradeRow">
+							<div class="col col1"><span class="done">Done</span></div>
+							<div class="col col2">Lướt sóng nhanh ETH khung M5</div>
+							<div class="col col3"><span class="green">Win</span></div>
+							<div class="col col4"><span class="green">10%</span></div>
+							<div class="col col5">John Nguyen</div>
+							<div class="col col6"><div class="point">4.5</div> <div class="comment"><span class="icon">12</span></div></div>
+						</div>
+                        <div class="clear"></div>
+                        <div class="paging floatRight">
+                          <a class="prev page-numbers" href="#">&laquo; Back</a>
+                          <a class='page-numbers current' href='#1'>1</a>
+                          <a class='page-numbers' href='#2'>2</a>
+                          <a class='page-numbers' href='#3'>3</a>
+                          <a class='page-numbers' href='#4'>4</a>
+                          <a class='page-numbers' href='#1'>5</a>
+                          <a class='page-numbers' href='#2'>6</a>
+                          <a class='page-numbers' href='#3'>7</a>
+                          <a class='page-numbers' href='#4'>8</a>
+                          <a class='page-numbers' href='#4'>9</a>
+                          <a class="next page-numbers" href="#">Next &raquo;</a>
+                        </div>
+					</div>
+
+				</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+        <!--FOOTER-->
+        <footer>
+            <div class="container">
+                <div class="subscribeBox">
+                    <img src="images/subscribe-title.png"/>
+                    <div class="subscribeForm">
+                        <form action="" method="">
+                            <input type="text" value="" placeholder="Your Email"/>
+                            <input type="submit" value="Submit" class="btn"/>
+                        </form>
+                    </div>
+                </div>
+                <div class="social">
+                    <img src="images/joinus.png"/>
+                    <div class="socialBox">
+                        <a href="#" class="fb"></a>
+                        <a href="#" class="tw"></a>
+                        <a href="#" class="fly"></a>
+                        <a href="#" class="cat"></a>
+                        <a href="#" class="message"></a>
+                    </div>
+                </div>
+                <div class="copyright">
+                    Copyright © 2017 . All rights reserved. Power by FAR EAST COMMERCIAL TRANSACTIONS COMPANY LTD
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+</html>
