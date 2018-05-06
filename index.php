e71ef362b9660f28446ac87e39d10520c2c9a176
lthanhbinh <lthanhbinh@tma.com.vn>
2018-05-06 11:03:50 +0700

    dsadsa

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <title>TRADE COIN CLUB</title>
 <meta name="description" content="">
 <meta name="author" content="">
-<meta name="viewport" content="width=640">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" href="css/all.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
